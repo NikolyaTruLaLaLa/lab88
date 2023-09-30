7bf0c4f3aa3e69d3d85011c4a8ed874ccf452906
Николай Игнатенко <1126ignatenkonikolay@gmail.com>
2023-09-30 10:38:30 +0300

    For each time different greetings

--- hw.pas
+++ hw.pas
@@ -1,2 +1,12 @@
-﻿##
-Print('Hello World!');
+﻿begin
+  var time := readInteger('What hour is now? -> ');
+  
+  assert((0 <= time) and (time <= 23), '0 <= time <= 23');
+ 
+  case time of
+    4..10: writeln('Доброе утро, мир!');
+    11..16: writeln('Добрый день, мир!');
+    17..22: writeln('Добрый вечер, мир!')
+    else writeln('Доброй ночи, мир!');
+  end;
+end.
