42e2cb4891a97b3eaf521bbe2b6adecfc47c9b67
Николай Игнатенко <1126ignatenkonikolay@gmail.com>
2023-09-30 11:09:29 +0300

    Мы научились переводитьб время в систему СИ !!!!!!!АААА!

---
+++ timer.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var hours := ReadInteger('Input hours -> ');
+  var minutes := ReadInteger('Input minutes -> ');
+  var seconds := ReadInteger('Input seconds -> ');
+  
+  assert((hours >= 0) and (minutes >= 0) and (seconds >= 0), 'отрицательное время? Давайте выберем точкой начала отсчёта рождение Христа, и пусть до этого момента ничего не существовало');
+  
+  Print($'Total quantity of seconds: {hours*3600 + minutes*60 + seconds}');
+  
+end.
