bc5ec5c700eefc1cf317c3b573c8702022d66697
Николай Игнатенко <1126ignatenkonikolay@gmail.com>
2023-09-30 10:55:13 +0300

    Теперь мы можем првоерять високосность года удалённо УРАААААА!!!1

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var year := ReadInteger('Input year  -> ');
+  
+  assert(year >= 0, 'year >= 0');
+  
+  var fl := false;
+  if (not(year.Divs(100)) or year.Divs(400)) and year.Divs(4) then fl := true;
+  
+  Print($'Этот год високосный? -> {fl}');
+end.
